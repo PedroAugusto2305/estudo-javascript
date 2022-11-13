9b528331fbfca20f1eb6dfc10efc6875740f1047
Pedro Augusto <p.augusto_23@outlook.com>
2022-11-13 15:44:23 -0300

    first commit

---
+++ tipos-primitivos/tipos-primitivos.js
@@ -0,0 +1,16 @@
+/* No javaScript (e em outras linguagens de programação) dados * * podem ser representados de diferentes formas.
+
+--------------------------------------------------------------------
+
+------------------------ Tipos primitivos ------------------------- 
+ 
+number e bigint (números grandes): números inteiros, float, e etc.
+
+string: são valores textuais, no JS números também podem ser usados como valores de texto. 
+
+boolean:
+
+null e undefined:
+
+
+*/
