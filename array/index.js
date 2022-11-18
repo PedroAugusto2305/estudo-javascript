7c09b31926fc3763e60e6b60181555282ae4a9af
Pedro Augusto <p.augusto_23@outlook.com>
2022-11-18 14:59:34 -0300

    arrays

---
+++ array/index.js
@@ -0,0 +1,9 @@
+const listaDeCompras = ['Açucar', false]
+console.log(listaDeCompras)
+
+listaDeCompras[0] = 'Arroz'
+listaDeCompras[1] = 'Feijão'
+listaDeCompras[2] = 7
+listaDeCompras[5] = 'Batata'
+listaDeCompras[4] = 'Macarrão'
+console.log(listaDeCompras)
