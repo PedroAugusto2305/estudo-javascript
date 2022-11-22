07e845c88991ebc8db8710d825eeb6ca2922c8a7
Pedro Augusto <p.augusto_23@outlook.com>
2022-11-22 15:20:57 -0300

    Anotacoes update

--- anotações/anotacoes.js
+++ anotações/anotacoes.js
@@ -272,4 +272,41 @@ Um array bidimensional é um array com duas dimensões, ou seja, é um array que
 sintaxe:
   let array = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
 
+--------------------------------------------------------------------------------
+
+----------------------------------- Objetos ------------------------------------
+
+É uma estrutura do javascript para se trabalhar com dicionários chave-valor.
+O par chave-valor em um bojeto javascript é chamado de propriedade:
+  => A chave é como um nome da propriedade;
+  => O valor é o valor que está armazenado nessa propriedade (semelhante aos valores armazenados em variáveis).
+
+Diferente dos arrays, seus elementos não possuem nenhuma sequência.
+Uma propriedade também pode ter strings, números e booleanos como chave.
+Uma propriedade pode armazenar qualquer tipo de dado como valor.
+Pode ser criado através de chaves {} => let objeto = {}
+
+Suas propriedades e funções podem ser referenciadas por encadeamento com ponto "." ou colchetes []
+  => objeto.prop = "Olá, Mundo!"
+      objeto.executar()
+  
+  let name = "prop"
+  objeto(name) = "Olá, mundo!"
+  objeto.prop === objeto[name]
+  let funcao = "executar"
+  objeto[funcao]()
+
+sintaxe:
+let objeto = {
+  nome: "Pedro",
+  idade: 26,
+  estudante: true
+  endereco: {
+    rua: "abc",
+    cep: 12345,
+    bairro: "abcd"
+  }
+}
+
+
 */
