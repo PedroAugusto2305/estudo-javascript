1ddff02a9a9e0c1fe339a48d04e75273423fc2f6
Pedro Augusto <p.augusto_23@outlook.com>
2022-11-30 13:41:23 -0300

    Anotacoes update

--- anotações/anotacoes.js
+++ anotações/anotacoes.js
@@ -308,5 +308,21 @@ let objeto = {
   }
 }
 
+--------------------------------------------------------------------------------
+
+----------------------------------- Funções ------------------------------------
+
+É uma estrutura do javascript que permite declarar um procedimento e executá-lo programaticamente.
+Uma função pode ser vista como: Entrada -> Procedimento -> Saída
+
+Tanto a entrada quanto a saída de uma função são opcionais, ou seja, uma função pode existir sem um deles ou sem ambos.
+As entradas da função são chamadas de parâmetros.
+As saídas da função são chamadas de retorno.
+
+Sintaxe:
+function nomeDaFuncao(entradas) {
+  bloco de código...
+  return saida
+}
 
 */
